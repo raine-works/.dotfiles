d44cfc4c49e701067838618360e906bfad047d91
raineworks <mail@raineworks.com>
2024-02-17 04:09:50 -0700

    adding setup script

---
+++ README.md
@@ -0,0 +1,5 @@
+# Get Started
+
+    shell```
+    curl -s https://raw.githubusercontent.com/raine-works/.dotfiles/scripts/setup.sh
+    ```
---
+++ scripts/setup.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+git clone https://github.com/raine-works/.dotfiles.git ~/.dotfiles
+bash ~/.dotfiles/install.sh
+bash ~/.dotfiles/sync.sh
+
