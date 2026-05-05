922dab24ff3f556355edacaf22d8dd9077844891
raine-works <raine@raineworks.com>
2026-05-05 15:30:48 -0600

    Refactor bun.sh to use explicit if for sourcing completions

--- shell/.config/shell/tools/bun.sh
+++ shell/.config/shell/tools/bun.sh
@@ -2,5 +2,9 @@
 # Bun — completions
 command -v bun >/dev/null 2>&1 || return 0
 
-# shellcheck source=/dev/null
-[ -s "$HOME/.bun/_bun" ] && source "$HOME/.bun/_bun"
+if [ -s "$HOME/.bun/_bun" ]; then
+    # shellcheck source=/dev/null
+    source "$HOME/.bun/_bun"
+fi
+
+return 0
